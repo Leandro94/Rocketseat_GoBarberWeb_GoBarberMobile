b3bcfd70692b5dadaa445701ec619bd76fb31435
LeandroR94 <reisleandro868@gmail.com>
2019-11-03 16:10:22 -0300

    Configurando rotas

---
+++ modulo09/src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+import SignUp from '../pages/SignUp';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/resgister" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
