17a04bdab620246c6ddf005ccd1bf648527827c3
LeandroR94 <reisleandro868@gmail.com>
2019-11-05 16:24:20 -0300

    Exibindo toasts

--- modulo09/src/store/modules/auth/reducer.js
+++ modulo09/src/store/modules/auth/reducer.js
@@ -13,7 +13,6 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = true;
         break;
       }
-
       case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
