118ae77fbadc01616fd9941ef6902edaba900353
LeandroR94 <reisleandro868@gmail.com>
2019-11-05 16:47:08 -0300

    Cadastro na aplicação

--- modulo09/src/store/modules/auth/sagas.js
+++ modulo09/src/store/modules/auth/sagas.js
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
+import SignUp from '~/pages/SignUp';
 
 export function* signIn({ payload }) {
   try {
@@ -25,4 +26,22 @@ export function* signIn({ payload }) {
     yield put(signFailure());
   }
 }
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+    yield put(signFailure());
+  }
+}
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/ISGN_UP_REQUEST', signUp),
+]);
