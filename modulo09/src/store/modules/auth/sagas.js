6746193d5e7b536c107447ab938f37a73a97cd61
LeandroR94 <reisleandro868@gmail.com>
2019-11-05 12:03:27 -0300

    Exibindo toasts

--- modulo09/src/store/modules/auth/sagas.js
+++ modulo09/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -14,12 +15,13 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
     //console.tron.log(user);
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
