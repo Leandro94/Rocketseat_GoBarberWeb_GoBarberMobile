e12a9f145ea10d9bdf953e43798885b79bbd17e0
LeandroR94 <reisleandro868@gmail.com>
2019-11-08 11:56:26 -0300

    Atualizando perfil

--- modulo09/src/store/modules/user/sagas.js
+++ modulo09/src/store/modules/user/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, put, call } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import { updateProfileFailure, updateProfileSuccess } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { nome, email, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      {
+        nome,
+        email,
+      },
+      rest.oldPassword ? rest : {}
+    );
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seus dados!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
