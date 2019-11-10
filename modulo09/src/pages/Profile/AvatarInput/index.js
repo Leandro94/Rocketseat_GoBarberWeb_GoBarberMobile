6b10e725f9347bc0e6801d6442dbcfb0990bdd24
LeandroR94 <reisleandro868@gmail.com>
2019-11-10 16:19:37 -0300

    Dados do Header

--- modulo09/src/pages/Profile/AvatarInput/index.js
+++ modulo09/src/pages/Profile/AvatarInput/index.js
@@ -19,7 +19,7 @@ export default function AvatarInput() {
         path: 'dataset.file',
       });
     }
-  }, [ref, registerField]);
+  }, [ref.current]);
 
   async function handleChange(e) {
     const data = new FormData();
