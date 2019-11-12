0a8b70128de510b04351072e35b92dd07a94149c
LeandroR94 <reisleandro868@gmail.com>
2019-11-12 14:11:59 -0300

    Estrutura configurada

--- modulo09/src/pages/Dashboard/index.js
+++ modulo09/src/pages/Dashboard/index.js
@@ -6,6 +6,7 @@ import {
   setHours,
   setMinutes,
   setSeconds,
+  setMilliseconds,
   isBefore,
   isEqual,
   parseISO,
@@ -34,15 +35,17 @@ export default function Dashboard() {
       });
       const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       const data = range.map(hour => {
-        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
         const compareDate = utcToZonedTime(checkDate, timezone);
 
         return {
           time: `${hour}:00h`,
           past: isBefore(compareDate, new Date()),
-          appointment: response.data.find(a =>
-            isEqual(parseISO(a.date), compareDate)
-          ),
+          appointment:
+            response.data === (a => isEqual(parseISO(a.date), compareDate)),
         };
       });
       setSchedule(data);
