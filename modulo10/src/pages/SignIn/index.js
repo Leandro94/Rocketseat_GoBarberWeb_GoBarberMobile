594c42b729321ff22c6753a08bd9483bdcbb891f
LeandroR94 <reisleandro868@gmail.com>
2019-11-14 10:13:26 -0300

    Input & Button

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
+import Button from '~/components/Button';
 
 // import { Container } from './styles';
 
@@ -9,6 +11,12 @@ export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 }
