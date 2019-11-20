ab129c7040157cc8bc59ac9499bbfc77a660a997
Leandro94 <reisleandro868@gmail.com>
2019-11-20 15:24:40 -0300

    Configurando background

--- modulo10/src/routes.js
+++ modulo10/src/routes.js
@@ -1,9 +1,9 @@
-import { createAppContainer, createSwithcNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 export default createAppContainer(
-  createSwithcNavigator({
+  createSwitchNavigator({
     SignIn,
     SignUp,
   })
