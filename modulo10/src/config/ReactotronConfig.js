348cfd6d4f3865fca5bc7f5738b43ff53d00ef57
Leandro94 <reisleandro868@gmail.com>
2019-11-21 10:00:48 -0300

    Configurando Redux

--- modulo10/src/config/ReactotronConfig.js
+++ modulo10/src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '172.16.8.214' })
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({ host: '172.16.8.214' })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
