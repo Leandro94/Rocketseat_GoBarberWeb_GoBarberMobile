6016de27a7c31bc5f11796482d80e6cab96678fa
Leandro94 <reisleandro868@gmail.com>
2019-11-22 12:06:24 -0300

    Estilizações das rotas

--- modulo10/src/pages/Profile/index.js
+++ modulo10/src/pages/Profile/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Agendamentos',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+
+Profile.navigationOptions = {
+  tabBarLabel: 'Meu perfil',
+  tabBarIcons: ({ tintColor }) => (
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
